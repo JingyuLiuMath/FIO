2e865039bc95c1ca2becb1a19ee36e50fd5ba158
JingyuLiu <381258337@qq.com>
2026-04-23 17:03:56 +0100

    update hss_rank 2d

---
+++ experiments/2d_const_amplitude/exp_2d_const_amplitude_hss_rank.m
@@ -0,0 +1,28 @@
+clear;
+close all;
+warning off;
+
+addpath('../../extern/FastBF.m/src');
+addpath('../../extern/FastBF.m/test/kernels');
+
+exp_phi_func = @(x, xi) fun0_2D(x, xi);
+
+p_list = (4 : 7)';
+num_n = length(p_list);
+
+for it_p = 1 : num_n
+    p = p_list(it_p);
+
+    n = 2^p;
+    N = n^2;
+
+    fprintf("\n\n\n\n");
+    fprintf("Basic info.\n");
+    fprintf("  p: %d\n", p);
+    fprintf("  n: %d\n", n);
+    fprintf("  N: %d\n", N);
+
+    result = run_FIO_hss_rank2D(exp_phi_func, n);
+    save("./data/hss_rank_" + string(p) + ".mat", ...
+        "result");
+end
---
+++ src/BF/run_FIO_hss_rank2D.m
@@ -0,0 +1,32 @@
+function result = run_FIO_hss_rank2D(...
+    exp_phi_func, n)
+
+N = n^2;
+
+result = struct();
+
+result.n = n;
+result.N = N;
+result.tol_list = [1e-2; 1e-4; 1e-6; 1e-8];
+result.rank_list = zeros(size(result.tol_list));
+half_n = n / 2;
+
+% Initialization.
+x_co = (0 : (n - 1))' / n;
+x = TensorProduct2D(x_co, x_co);
+
+xi_co1 = (-half_n : -1)';
+xi_co2 = (0 : (half_n - 1))';
+
+xi_row = TensorProduct2D(xi_co1, xi_co1);
+xi_col = TensorProduct2D(xi_co1, xi_co2);
+
+sub_K = exp_phi_func(x, xi_row)' * exp_phi_func(x, xi_col);
+sigma = svd(sub_K);
+for it = 1 : size(result.tol_list, 1)
+    result.rank_list(it) = find(sigma >= result.tol_list(it) * sigma(1), 1, "last");
+    fprintf("tol: %.1e, rank: %d\n", result.tol_list(it), result.rank_list(it));
+end
+
+
+end
