a7e7117ecde527d1c3fc074c73f33c4d42fc0c93
JingyuLiu <381258337@qq.com>
2026-05-18 11:09:19 -0400

    update FIO2d

---
+++ src/Others/getMaxAvailableMemory.m
@@ -0,0 +1,25 @@
+function memGB = getMaxAvailableMemory()
+
+if ispc
+    mem = memory;
+    memGB = mem.MemAvailableAllArrays / 1024^3;    
+elseif unix
+    [status, result] = system('cat /proc/meminfo | grep MemAvailable');
+    if status == 0
+        tokens = regexp(result, '(\d+)\s*kB', 'tokens');
+        if ~isempty(tokens)
+            kb = str2double(tokens{1}{1});
+            memGB = kb / 1024^2;  % kB -> GB
+        else
+            memGB = NaN;
+        end
+    else
+        memGB = NaN;
+    end
+else
+    memGB = NaN;
+end
+
+memGB = memGB * 0.7;
+
+end
