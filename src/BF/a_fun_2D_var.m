3c22c3db47b49ce57b9f9212b799144e95ef08cf
JingyuLiu <381258337@qq.com>
2026-05-28 11:19:35 +0100

    update fio

--- src/BF/a_fun_2D_var.m
+++ src/BF/a_fun_2D_var.m
@@ -5,9 +5,6 @@ factor = 16;
 c1_func = @(x1, x2) (2 + sin(2 * pi * x1) .* sin(2 * pi * x2)) / factor;
 c2_func = @(x1, x2) (2 + cos(2 * pi * x1) .* cos(2 * pi * x2)) / factor;
 
-% rho_func = @(x1, x2, xi1, xi2) sqrt(...
-%     c1_func(x1, x2).^2 * (xi1.^2)' ...
-%     + c2_func(x1, x2).^2 * (xi2.^2)');
 rho_func = @(x1, x2, xi1, xi2) c1_func(x1, x2) * sqrt((xi1.^2)' + (xi2.^2)');
 
 phi_nonlin = rho_func(x(:, 1), x(:, 2), xi(:, 1), xi(:, 2));
