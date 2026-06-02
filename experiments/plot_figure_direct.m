d9ff0d7e5fe8507ffd9a3cf221a1f48f7f7d7fb4
Jingyu Liu <381258337@qq.com>
2026-06-03 00:59:35 +0800

    update plot and print

--- experiments/plot_figure_direct.m
+++ experiments/plot_figure_direct.m
@@ -35,29 +35,34 @@ for it_n = 1 : num_n
     N_list(it_n) = curr_result.N;
 end
 
+% ========== Rank ==========
 figure();
 xlabel_name = "$N$";
+ylabel_name = "numerical rank";
 
 title_name = "Rank";
 my_name = "_hss_rank";
 figure_name = figure_prefix + my_name;
 
+color_rank = "#0072BD";
+color_rank_ref = "#D95319";
+
 t_list = hss_rank_list;
 marker_list = ["x"];
 display_name_list = ["hss rank"];
-plot_single_curve(N_list, t_list, marker_list, display_name_list);
+plot_single_curve(N_list, t_list, marker_list, display_name_list, color_rank);
 
 if isfield(result_list(1), "n")
     scaling_type = "$O(\sqrt{N})$";
     factor = mean(hss_rank_list);
 else
     scaling_type = "$O(1)$";
-    factor = 16;  % const
-    % factor = 30;  % var
+    factor = ceil(mean(hss_rank_list) * 1.1);
 end
-plot_ref_curve(N_list, scaling_type, factor);
+plot_ref_curve(N_list, scaling_type, factor, color_rank_ref);
 
 xlabel(xlabel_name, "Interpreter", "latex");
+ylabel(ylabel_name, "Interpreter", "latex");
 if isunix
     if isfield(result_list(1), "n")
         xlim([1e3 1e6]);
@@ -65,17 +70,17 @@ if isunix
         ylim([128 4096]);
         yticks([256 512 1024 2048]);
     else
-        % ylim([10 13]);
-        % yticks([10 11 12 13]);
+        ylim([min(hss_rank_list) ceil(max(hss_rank_list) * 1.2)]);
     end
 end
-title(title_name, "Interpreter", "latex");
-legend("Location", "southeast", "Interpreter", "latex");
+% title(title_name, "Interpreter", "latex");
+legend("Location", "southeast", "Interpreter", "latex", "NumColumns", 2);
 set(gca, 'FontSize', 24);
 saveas(gcf, figure_name + ".png", "png");
 saveas(gcf, figure_name + ".eps", "epsc");
 
-figure('Position', [100 100 900 700]);
+% ========== Time scaling ==========
+figure();
 xlabel_name = "$N$";
 ylabel_name = "time (s)";
 
@@ -83,15 +88,32 @@ title_name = "Time scaling";
 my_name = "_time_scaling";
 figure_name = figure_prefix + my_name;
 
-marker_list = ["o", "+", "*", "x", "square"];
+marker_list = ["o", "+", "*", "x"];
 
-apply_bf_flag = 1;
+color_cBF = "#0072BD";
+color_cBF_ref = "b";
+
+color_aBF = "#D95319";
+color_aBF_ref = "r";
+
+color_cHSS = "#D95319";
+color_cHSS_ref = "r";
+
+color_fHSS = "#7E2F8E";
+color_fHSS_ref = "m";
+
+color_solve = "#77AC30";
+color_solve_ref = "c";
+
+apply_bf_flag = 0;
 
 if apply_bf_flag == 1
     t_list = [t_construct_BF_list, ...
         t_apply_BF_list];
     display_name_list = ["$t_{\mathrm{cBF}}$", ...
         "$t_{\mathrm{aBF}}$"];
+    color_list = [color_cBF, ...
+        color_aBF];
 else
     t_list = [t_construct_BF_list, ...
         t_construct_HSS_list, ...
@@ -101,24 +123,28 @@ else
         "$t_{\mathrm{cHSS}}$", ...
         "$t_{\mathrm{fHSS}}$", ...
         "$t_{\mathrm{sHSS}}$"];
+    color_list = [color_cBF, ...
+        color_cHSS, ...
+        color_fHSS, ...
+        color_solve];
 end
 
-plot_single_curve(N_list, t_list, marker_list, display_name_list);
+plot_single_curve(N_list, t_list, marker_list, display_name_list, color_list);
 
 % Construct BF.
 scaling_type = "$O(N \log N)$";
 factor = mean(t_construct_BF_list);
-plot_ref_curve(N_list, scaling_type, factor);
+plot_ref_curve(N_list, scaling_type, factor, color_cBF_ref);
 
 % Apply BF.
 if apply_bf_flag == 1
     scaling_type = "$O(N \log N)$";
     factor = mean(t_apply_BF_list);
-    plot_ref_curve(N_list, scaling_type, factor);
+    plot_ref_curve(N_list, scaling_type, factor, color_aBF_ref);
 
     scaling_type = "$O(N^{1.5} \log N)$";
     factor = mean(t_apply_BF_list);
-    plot_ref_curve(N_list, scaling_type, factor);
+    plot_ref_curve(N_list, scaling_type, factor, color_aBF_ref);
 end
 
 if apply_bf_flag == 0
@@ -130,7 +156,7 @@ if apply_bf_flag == 0
         scaling_type = "$O(N \log^{2} N)$";
     end
     factor = mean(t_construct_HSS_list);
-    plot_ref_curve(N_list, scaling_type, factor);
+    plot_ref_curve(N_list, scaling_type, factor, color_cHSS_ref);
 
     % Factor HSS.
     if isfield(result_list(1), "n")
@@ -139,7 +165,7 @@ if apply_bf_flag == 0
         scaling_type = "$O(N)$";
     end
     factor = mean(t_factor_HSS_list);
-    plot_ref_curve(N_list, scaling_type, factor);
+    plot_ref_curve(N_list, scaling_type, factor, color_fHSS_ref);
 
     % Solve
     if isfield(result_list(1), "n")
@@ -148,9 +174,8 @@ if apply_bf_flag == 0
         scaling_type = "$O(N)$";
     end
     factor = mean(t_solve_list);
-    plot_ref_curve(N_list, scaling_type, factor);
+    plot_ref_curve(N_list, scaling_type, factor, color_solve_ref);
 end
-
 
 xlabel(xlabel_name, "Interpreter", "latex");
 ylabel(ylabel_name, "Interpreter", "latex");
@@ -161,18 +186,20 @@ if isunix
     else
         xlim([2^9 2^20])
         xticks([1e3 1e4 1e5 1e6])
-        ylim([1e-3 1e3]);
-        yticks([1e-3 1e-2 1e-1 1e0 1e1 1e2 1e3]);
+        ylim([1e-4 1e3]);
+        yticks([1e-3 1e-1 1e1 1e3]);
     end
 end
-title(title_name, "Interpreter", "latex");
-legend("Location", "southeastoutside", "Interpreter", "latex");
-set(gca, 'FontSize', 32);
+% title(title_name, "Interpreter", "latex");
+
+lgd = legend("Location", "southeast", "Interpreter", "latex", "NumColumns", 2);
+set(gca, 'FontSize', 24);
 saveas(gcf, figure_name + ".png", "png");
 saveas(gcf, figure_name + ".eps", "epsc");
 
-
+% ========== Relative error ==========
 figure();
+set(gca, 'Position', [0.13 0.11 0.775 0.815]);
 xlabel_name = "$N$";
 
 title_name = "Relative error";
@@ -186,7 +213,8 @@ marker_list = ["o", "+", "*", "x"];
 display_name_list = ["$e_{\mathrm{BF}}$", ...
     "$e_{\mathrm{HSS}}$", ...
     "$e_{\mathrm{s}}$"];
-plot_single_curve(N_list, t_list, marker_list, display_name_list);
+color_list = ["#0072BD", "#7E2F8E", "#77AC30"];
+plot_single_curve(N_list, t_list, marker_list, display_name_list, color_list);
 
 xlabel(xlabel_name, "Interpreter", "latex");
 title(title_name, "Interpreter", "latex");
@@ -197,7 +225,7 @@ saveas(gcf, figure_name + ".eps", "epsc");
 
 end
 
-function plot_single_curve(N_list, t_list, marker_list, display_name_list)
+function plot_single_curve(N_list, t_list, marker_list, display_name_list, color_list)
 
 num_param = size(t_list, 2);
 
@@ -206,13 +234,14 @@ for it_param = 1 : num_param
         "LineWidth", 2, ...
         "Marker", marker_list(it_param), ...
         "Markersize", 12, ...
+        "Color", color_list(it_param), ...
         "DisplayName", display_name_list(it_param));
     hold on;
 end
 
 end
 
-function plot_ref_curve(N_list, scaling_type, factor)
+function plot_ref_curve(N_list, scaling_type, factor, color)
 
 switch scaling_type
     case ""
@@ -220,24 +249,31 @@ switch scaling_type
     case "$O(1)$"
         ref_line = ones(size(N_list));
         ref_line = ref_line / mean(ref_line) * factor;
+        linestyle = "--";
     case "$O(N)$"
         ref_line = N_list;
         ref_line = ref_line / mean(ref_line) * factor;
+        linestyle = ":";
     case "$O(\sqrt{N})$"
         ref_line = sqrt(N_list);
         ref_line = ref_line / mean(ref_line) * factor;
+        linestyle = "--";
     case "$O(N \log N)$"
         ref_line = N_list .* log2(N_list);
         ref_line = ref_line / mean(ref_line) * factor;
+        linestyle = "--";
     case "$O(N \log^{2} N)$"
         ref_line = N_list .* (log2(N_list).^2);
         ref_line = ref_line / mean(ref_line) * factor;
+        linestyle = "-.";
     case "$O(N^{1.5})$"
         ref_line = N_list.^(1.5);
         ref_line = ref_line / mean(ref_line) * factor;
+        linestyle = ":";
     case "$O(N^{1.5} \log N)$"
         ref_line = N_list.^(1.5) .* log2(N_list);
         ref_line = ref_line / mean(ref_line) * factor;
+        linestyle = "-.";
     case "$O(N^{2})$"
         ref_line = N_list.^(2);
         ref_line = ref_line / mean(ref_line) * factor;
@@ -245,8 +281,9 @@ end
 
 loglog(N_list, ref_line, ...
     "LineWidth", 2, ...
-    "LineStyle", "--", ...
-    "DisplayName", scaling_type);
+    "LineStyle", linestyle, ...
+    "DisplayName", scaling_type, ...
+    "Color", color);
 hold on;
 
 end
---
+++ experiments/print_table_direct_without_tol.m
@@ -0,0 +1,69 @@
+function print_table_direct_without_tol(result_list, ...
+    caption_name, label_name)
+
+num_n = size(result_list, 1);
+
+fprintf("\n");
+
+fprintf("\\begin{table}[tbhp]\n")
+fprintf("\\centering\n")
+fprintf("\\begin{tabular}{c | cc | cc | ccc}\n")
+fprintf("\\toprule\n")
+
+fprintf("\\(N\\) ");
+
+fprintf("& \\(t_{\\mathrm{cBF}}\\) ");
+fprintf("& \\(e_{\\mathrm{BF}}\\) ");
+
+fprintf("& \\(t_{\\mathrm{cHSS}}\\) ");
+fprintf("& \\(e_{\\mathrm{HSS}}\\) ");
+
+fprintf("& \\(t_{\\mathrm{fHSS}}\\) ");
+fprintf("& \\(t_{\\mathrm{sHSS}}\\) ");
+fprintf("& \\(e_{\\mathrm{s}}\\) ");
+
+fprintf("\\\\ \n");
+for it_n = 1 : num_n
+    curr_result = result_list(it_n, :);
+    print_result_direct(curr_result);
+end
+fprintf("\\bottomrule\n");
+fprintf("\\end{tabular}\n");
+fprintf("\\caption{%s}\n", caption_name);
+fprintf("\\label{tab:%s}\n", label_name);
+fprintf("\\end{table}\n");
+fprintf("\n");
+
+
+end
+
+function print_result_direct(result_list)
+
+num_hss_tol = 1;
+fprintf("\\midrule\n");
+if isfield(result_list, "n")
+    n = result_list.n;
+    tmp_str = string(n) + "^{2}";
+else
+    N = result_list.N;
+    p = log2(N);
+    tmp_str = "2^{" + string(p) + "}";
+end
+fprintf("\\(%s\\) ", tmp_str);
+
+
+for it_hss_tol = 1 : num_hss_tol    
+    fprintf("& %.1e ", result_list.t_construct_BF);
+    fprintf("& %.1e ", result_list.rel_err_BF);
+    
+    fprintf("& %.1e ", result_list.t_construct_HSS);
+    fprintf("& %.1e ", result_list.rel_err_HSS);
+    
+    fprintf("& %.1e ", result_list.t_factor_HSS);
+    fprintf("& %.1e ", result_list.t_direct);
+    fprintf("& %.1e ", result_list.rel_err_direct);
+    
+    fprintf("\\\\ \n");
+end
+
+end
