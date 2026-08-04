53af5b87012357f81f58b41ee6b766a29a6aa3b6
JingyuLiu <381258337@qq.com>
2026-08-04 09:48:38 +0100

    update hss_cg to cg

--- src/BF/run_FIO1D_inv_CG.m
+++ src/BF/run_FIO1D_inv_CG.m
@@ -1,13 +1,11 @@
-function result = run_FIO2D_inv_HSS_CG(...
-    n, ...
+function result = run_FIO1D_inv_CG(...
+    N, ...
     a_func, phi_func, ...
     n_leaf_bf, r_bf, tol_bf, type_bf, ...
     num_sample, ...
     tol_cg, maxit_cg)
 
-N = n^2;
 fprintf("Basic info.\n");
-fprintf("  n: %d\n", n);
 fprintf("  N: %d\n", N);
 
 fprintf("  n_leaf_bf: %d\n", n_leaf_bf);
@@ -20,7 +18,6 @@ fprintf("  maxit_cg: %d\n", maxit_cg);
 
 result = struct();
 
-result.n = n;
 result.N = N;
 result.n_leaf_bf = n_leaf_bf;
 result.r_bf = r_bf;
@@ -30,13 +27,11 @@ result.num_sample = num_sample;
 result.tol_cg = tol_cg;
 result.maxit_cg = maxit_cg;
 
-half_n = n / 2;
+half_N = N / 2;
 
 % Initialization.
-x_co = (0 : (n - 1))' / n;
-x = TensorProduct2D(x_co, x_co);
-xi_co = (-half_n : (half_n - 1))';
-xi = TensorProduct2D(xi_co, xi_co);
+x = (0 : (N - 1))' / N;
+xi = (-half_N : (half_N - 1))';
 
 exp_phi_func = @(x, xi) complex(...
     cos(2 * pi * phi_func(x, xi)), ...
@@ -45,18 +40,18 @@ k_func = @(x, xi) a_func(x, xi) .* exp_phi_func(x, xi);
 
 % BF.
 fprintf("BF.\n");
-tic;
+t_construct_BF_start = tic;
 result.K_BF = my_construct_bf(...
-    n, a_func, phi_func, ...
+    N, a_func, phi_func, ...
     x, xi, ...
     n_leaf_bf, r_bf, tol_bf, type_bf);
-result.t_construct_BF = toc;
+result.t_construct_BF = toc(t_construct_BF_start);
 fprintf("  t_construct_BF: %.1e\n", result.t_construct_BF);
 
 result.f_ex = randn(N, 1) + 1i * randn(N, 1);
-tic;
+t_apply_BF_start = tic;
 result.Kf_ex = my_apply_bf(result.K_BF, result.f_ex, type_bf);
-result.t_apply_BF = toc;
+result.t_apply_BF = toc(t_apply_BF_start);
 
 fprintf("  t_apply_BF: %.1e\n", result.t_apply_BF);
 result.rel_err_BF = fbf_check(N, k_func, result.f_ex, x, xi, result.Kf_ex, num_sample);
@@ -69,9 +64,9 @@ rhs = my_apply_bf_adj(result.K_BF, result.Kf_ex, type_bf);
 % Iterative solution.
 fprintf("Iterative solution.\n");
 fprintf("  Without precond.\n");
-tic;
+t_cg_start = tic;
 [f_cg, result.flag_cg, ~, result.iter_cg] = pcg(op_G, rhs, tol_cg, maxit_cg);
-result.t_cg = toc;
+result.t_cg = toc(t_cg_start);
 
 fprintf("    t_cg: %.1e\n", result.t_cg);
 fprintf("    iter_cg: %d\n", result.iter_cg);
