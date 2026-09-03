df30803b472ff12369fc3468988a54fadee1b1ed
Jingyu Liu <381258337@qq.com>
2026-09-04 05:36:15 +0800

    update plot

--- experiments/fio_inv_hss/plot_figure_direct.m
+++ experiments/fio_inv_hss/plot_figure_direct.m
@@ -35,21 +35,24 @@ for it_n = 1 : num_n
     N_list(it_n) = curr_result.N;
 end
 
-figure_position = [100 100 1600 900];
-paper_position = [0 0 24 12];
-paper_size = paper_position(3:4);
-if isfield(result_list(1), "n")
-    axes_position = [0.18 0.23 0.55 0.70];
-else
-    axes_position = [0.13 0.23 0.60 0.70];
-end
-font_size = 64;
+figure_position = [100, 100, 1000, 500];
+figure_position_time = [100, 100, 1200, 500];
+time_width_scale = figure_position(3) / figure_position_time(3);
+axes_position = [0.16, 0.18, 0.58, 0.74];
+axes_position_time = [ ...
+    axes_position(1) * time_width_scale, ...
+    axes_position(2), ...
+    axes_position(3) * time_width_scale, ...
+    axes_position(4)];
+font_size = 32;
 font_size_rank = font_size;
 font_size_scaling = font_size;
+font_size_lgd = font_size;
 
 % ========== Rank ==========
-figure();
-set(gcf, 'Position', figure_position);
+fig_rank = figure();
+set(fig_rank, "Position", figure_position);
+% set(gcf, 'Position', figure_position);
 xlabel_name = "$N$";
 ylabel_name = "numerical rank";
 
@@ -62,49 +65,80 @@ color_rank_ref = "#D95319";
 
 t_list = hss_rank_list;
 marker_list = ["x"];
-display_name_list = ["hss rank"];
+display_name_list = ["HSS rank"];
 plot_single_curve(N_list, t_list, marker_list, display_name_list, color_rank);
 
 if isfield(result_list(1), "n")
     scaling_type = "$O(\sqrt{N})$";
-    factor = mean(hss_rank_list);
+    switch figure_prefix
+        case "./figure/2d_const_amplitude"
+            factor = mean(hss_rank_list) * 0.8;
+        case "./figure/2d_var_amplitude"
+            factor = mean(hss_rank_list) * 0.8;
+    end
+    plot_ref_curve(N_list, scaling_type, factor, color_rank_ref);
 else
     scaling_type = "$O(1)$";
-    factor = 25;
+    switch figure_prefix
+        case "./figure/1d_const_amplitude"
+            % factor = 25;
+        case  "./figure/1d_var_amplitude"
+            factor = 25;
+    end
+    % plot_ref_curve(N_list, scaling_type, factor, color_rank_ref);
 end
-plot_ref_curve(N_list, scaling_type, factor, color_rank_ref);
 
 xlabel(xlabel_name, "Interpreter", "latex");
 ylabel(ylabel_name, "Interpreter", "latex");
 if isunix
     if isfield(result_list(1), "n")
-        xlim([1e3 1e6]);
-        xticks([1e3 1e4 1e5 1e6]);
-        ylim([128 4096]);
-        yticks([256 512 1024 2048]);
+        switch figure_prefix
+            case "./figure/2d_const_amplitude"
+                xlim([1e3 1e6]);
+                xticks([1e3 1e4 1e5 1e6]);
+                ylim([128 4096]);
+                yticks([256 512 1024 2048]);
+            case  "./figure/2d_var_amplitude"
+                xlim([1e3 1e6]);
+                xticks([1e3 1e4 1e5 1e6]);
+                ylim([128 4096]);
+                yticks([256 512 1024 2048]);
+        end
     else
-        xlim([2^9 2^19]);
-        xticks([1e3 1e4 1e5 1e6]);
-        ylim([18 26]);
-        yticks([18 20 22 24 26]);
+        switch figure_prefix
+            case "./figure/1d_const_amplitude"
+                % xlim([2^9 2^19]);
+                % xticks([1e3 1e4 1e5 1e6]);
+                % ylim([18 26]);
+                % yticks([18 20 22 24 26]);
+            case "./figure/1d_var_amplitude"
+                xlim([2^9 2^19]);
+                xticks([1e3 1e4 1e5 1e6]);
+                ylim([18 26]);
+                yticks([18 20 22 24 26]);
+        end
     end
 end
 % title(title_name, "Interpreter", "latex");
-legend("Location", "southeast", "Interpreter", "latex");
-set(gca, ...
-    'FontSize', font_size_rank, ...
-    'Units', 'normalized', ...
-    'Position', axes_position);
-set(gcf, 'PaperUnits', 'inches');
-set(gcf, 'PaperPosition', paper_position);
-set(gcf, 'PaperSize', paper_size);
-set(gcf, 'PaperPositionMode', 'manual');
-print(gcf, figure_name + ".png", "-dpng", "-r200");
-print(gcf, figure_name + ".pdf", "-dpdf", "-vector");
+ax_rank = gca;
+set(ax_rank, ...
+    "Units", "normalized", ...
+    "Position", axes_position, ...
+    "FontSize", font_size_rank);
+legend(ax_rank, ...
+    "Location", "southeast", ...
+    "Interpreter", "latex", ...
+    "FontSize", font_size_lgd);
+
+drawnow;
+exportgraphics(fig_rank, figure_name + ".pdf", ...
+    "ContentType", "vector");
+exportgraphics(fig_rank, figure_name + ".png", ...
+    "Resolution", 300);
 
 % ========== Time scaling ==========
-figure();
-set(gcf, 'Position', figure_position);
+fig_time = figure();
+set(fig_time, "Position", figure_position_time);
 xlabel_name = "$N$";
 ylabel_name = "time (s)";
 
@@ -164,17 +198,40 @@ plot_single_curve(N_list, t_list, marker_list, display_name_list, color_list);
 if apply_bf_flag == 1
     % Construct BF.
     scaling_type = "$O(N \log N)$";
-    factor = mean(t_construct_BF_list);
+    switch figure_prefix
+        case "./figure/1d_const_amplitude"
+            factor = mean(t_construct_BF_list);
+        case "./figure/1d_var_amplitude"
+            factor = mean(t_construct_BF_list);
+        case "./figure/2d_const_amplitude"
+            factor = mean(t_construct_BF_list);
+        case "./figure/2d_var_amplitude"
+            factor = mean(t_construct_BF_list);
+    end
     plot_ref_curve(N_list, scaling_type, factor, color_cBF_ref);
 
     % Apply BF.
     scaling_type = "$O(N \log N)$";
-    factor = mean(t_apply_BF_list);
+    switch figure_prefix
+        case "./figure/1d_const_amplitude"
+            factor = mean(t_apply_BF_list);
+        case "./figure/1d_var_amplitude"
+            factor = mean(t_apply_BF_list);
+        case "./figure/2d_const_amplitude"
+            factor = mean(t_apply_BF_list);
+        case "./figure/2d_var_amplitude"
+            factor = mean(t_apply_BF_list);
+    end
     plot_ref_curve(N_list, scaling_type, factor, color_aBF_ref);
 
     if isfield(result_list(1), "n")
         scaling_type = "$O(N^{1.5})$";
-        factor = mean(t_apply_BF_list);
+        switch figure_prefix
+            case "./figure/2d_const_amplitude"
+                factor = mean(t_apply_BF_list);
+            case "./figure/2d_var_amplitude"
+                factor = mean(t_apply_BF_list);
+        end
         plot_ref_curve(N_list, scaling_type, factor, color_aBF_ref_act);
     end
 end
@@ -182,41 +239,89 @@ end
 if apply_bf_flag == 0
     % Construct BF.
     scaling_type = "$O(N \log N)$";
-    factor = mean(t_construct_BF_list);
+    switch figure_prefix
+        case "./figure/1d_const_amplitude"
+            factor = mean(t_construct_BF_list);
+        case "./figure/1d_var_amplitude"
+            factor = mean(t_construct_BF_list);
+        case "./figure/2d_const_amplitude"
+            factor = mean(t_construct_BF_list) * 0.05;
+        case "./figure/2d_var_amplitude"
+            factor = mean(t_construct_BF_list) * 0.05;
+    end
     plot_ref_curve(N_list, scaling_type, factor, color_cBF_ref);
 
     % Construct HSS.
     if isfield(result_list(1), "n")
         scaling_type = "$O(N^{1.5} \log N)$";
-        factor = mean(t_construct_HSS_list) * 0.8;
+        switch figure_prefix
+            case "./figure/2d_const_amplitude"
+                factor = mean(t_construct_HSS_list) * 0.02;
+            case "./figure/2d_var_amplitude"
+                factor = mean(t_construct_HSS_list) * 0.02;
+        end
         plot_ref_curve(N_list, scaling_type, factor, color_cHSS_ref);
 
         scaling_type = "$O(N^{2})$";
-        factor = mean(t_construct_HSS_list);
+        switch figure_prefix
+            case "./figure/2d_const_amplitude"
+                factor = mean(t_construct_HSS_list) * 0.05;
+            case "./figure/2d_var_amplitude"
+                factor = mean(t_construct_HSS_list) * 0.05;
+        end
         plot_ref_curve(N_list, scaling_type, factor, color_cHSS_ref_act);
     else
         scaling_type = "$O(N \log^{2} N)$";
-        factor = mean(t_construct_HSS_list);
+        switch figure_prefix
+            case "./figure/1d_const_amplitude"
+                factor = mean(t_construct_HSS_list);
+            case "./figure/1d_var_amplitude"
+                factor = mean(t_construct_HSS_list);
+        end
         plot_ref_curve(N_list, scaling_type, factor, color_cHSS_ref);
     end
 
     % Factor HSS.
     if isfield(result_list(1), "n")
         scaling_type = "$O(N^{1.5})$";
+        switch figure_prefix
+            case "./figure/2d_const_amplitude"
+                factor = mean(t_factor_HSS_list);
+            case "./figure/2d_var_amplitude"
+                factor = mean(t_factor_HSS_list);
+        end
+        plot_ref_curve(N_list, scaling_type, factor, color_fHSS_ref);
     else
         scaling_type = "$O(N)$";
+        switch figure_prefix
+            case "./figure/1d_const_amplitude"
+                factor = mean(t_factor_HSS_list) * 0.5;
+            case "./figure/1d_var_amplitude"
+                factor = mean(t_factor_HSS_list) * 0.5;
+        end
+        plot_ref_curve(N_list, scaling_type, factor, color_fHSS_ref);
     end
-    factor = mean(t_factor_HSS_list);
-    plot_ref_curve(N_list, scaling_type, factor, color_fHSS_ref);
 
     % Solve HSS.
     if isfield(result_list(1), "n")
-        scaling_type = "$O(N \log N)$";
+        % scaling_type = "$O(N \log N)$";
+        % switch figure_prefix
+        %     case "./figure/2d_const_amplitude"
+        %         factor = mean(t_solve_list);
+        %     case "./figure/2d_var_amplitude"
+        %         factor = mean(t_solve_list);
+        % end
+        % plot_ref_curve(N_list, scaling_type, factor, color_solve_ref);
     else
-        scaling_type = "$O(N)$";
+        % scaling_type = "$O(N)$";
+        % switch figure_prefix
+        %     case "./figure/1d_const_amplitude"
+        %        factor = mean(t_solve_list);
+        %     case "./figure/1d_var_amplitude"
+        %         factor = mean(t_solve_list);
+        % end
+        % plot_ref_curve(N_list, scaling_type, factor, color_solve_ref);
     end
-    factor = mean(t_solve_list);
-    plot_ref_curve(N_list, scaling_type, factor, color_solve_ref);
 end
 
 xlabel(xlabel_name, "Interpreter", "latex");
@@ -226,31 +331,82 @@ if isunix
         xlim([1e3 1e6]);
         xticks([1e3 1e4 1e5 1e6]);
         if apply_bf_flag == 1
-            ylim([1e-3 1e3]);
-            yticks([1e-2 1e-1 1e0 1e1 1e2 1e3]);
+            switch figure_prefix
+                case "./figure/2d_const_amplitude"
+                    ylim([1e-3 1e3]);
+                    yticks([1e-2 1e-1 1e0 1e1 1e2 1e3]);
+                case "./figure/2d_var_amplitude"
+                    ylim([1e-3 1e3]);
+                    yticks([1e-2 1e-1 1e0 1e1 1e2 1e3]);
+            end
         else
-            ylim([1e-2 1e6]);
-            yticks([1e-2 1e0 1e2 1e4 1e6]);
+            switch figure_prefix
+                case "./figure/2d_const_amplitude"
+                    ylim([1e-2 1e6]);
+                    yticks([1e-2 1e0 1e2 1e4 1e6]);
+                case "./figure/2d_var_amplitude"
+                    ylim([1e-2 1e6]);
+                    yticks([1e-2 1e0 1e2 1e4 1e6]);
+            end
         end
     else
-        xlim([2^9 2^19]);
-        xticks([1e3 1e4 1e5 1e6]);
-        ylim([1e-3 1e3]);
-        yticks([1e-3 1e-1 1e1 1e3]);
+        switch figure_prefix
+            case "./figure/1d_const_amplitude"
+                xlim([2^9 2^19]);
+                xticks([1e3 1e4 1e5 1e6]);
+                ylim([1e-3 1e3]);
+                yticks([1e-3 1e-2 1e-1 1e0 1e1 1e2 1e3]);
+            case "./figure/1d_var_amplitude"
+                xlim([2^9 2^19]);
+                xticks([1e3 1e4 1e5 1e6]);
+                ylim([1e-3 1e3]);
+                yticks([1e-3 1e-2 1e-1 1e0 1e1 1e2 1e3]);
+        end
     end
 end
 % title(title_name, "Interpreter", "latex");
-legend("Location", "eastoutside", "Interpreter", "latex", "NumColumns", 1);
-set(gca, ...
-    'FontSize', font_size_scaling, ...
-    'Units', 'normalized', ...
-    'Position', axes_position);
-set(gcf, 'PaperUnits', 'inches');
-set(gcf, 'PaperPosition', paper_position);
-set(gcf, 'PaperSize', paper_size);
-set(gcf, 'PaperPositionMode', 'manual');
-print(gcf, figure_name + ".png", "-dpng", "-r200");
-print(gcf, figure_name + ".pdf", "-dpdf", "-vector");
+ax_time = gca;
+set(ax_time, ...
+    "Units", "normalized", ...
+    "Position", axes_position_time, ...
+    "FontSize", font_size_scaling);
+
+% eastoutside 会自动缩小 axes
+lgd = legend(ax_time, ...
+    "Location", "eastoutside", ...
+    "Interpreter", "latex", ...
+    "FontSize", font_size_lgd);
+
+drawnow;
+
+% 创建 legend 后，恢复和 rank 完全相同的 axes 大小
+set(ax_time, "Position", axes_position_time);
+
+% 手动把 legend 放在右侧空白区域
+set(lgd, "Units", "normalized");
+drawnow;
+
+lgd_position = get(lgd, "Position");
+
+legend_gap = 0.02;
+extra_width = 0.05;
+
+% 给 LaTeX 公式和右侧内边距增加空间
+lgd_position(3) = lgd_position(3) + extra_width;
+
+% 放在 axes 右侧，而不是紧贴 Figure 右边界
+axes_right = axes_position_time(1) + axes_position_time(3);
+lgd_position(1) = axes_right + legend_gap;
+lgd_position(2) = 0.5 - lgd_position(4) / 2;
+
+set(lgd, "Position", lgd_position);
+drawnow;
+
+drawnow;
+exportgraphics(fig_time, figure_name + ".pdf", ...
+    "ContentType", "vector");
+exportgraphics(fig_time, figure_name + ".png", ...
+    "Resolution", 300);
 
 end
 
@@ -260,7 +416,7 @@ num_param = size(t_list, 2);
 
 for it_param = 1 : num_param
     loglog(N_list, t_list(:, it_param), ...
-        "LineWidth", 8, ...
+        "LineWidth", 4, ...
         "Marker", marker_list(it_param), ...
         "Markersize", 16, ...
         "Color", color_list(it_param), ...
@@ -310,7 +466,7 @@ switch scaling_type
 end
 
 loglog(N_list, ref_line, ...
-    "LineWidth", 8, ...
+    "LineWidth", 4, ...
     "LineStyle", linestyle, ...
     "DisplayName", scaling_type, ...
     "Color", color);
